b22852cab100f8d5399211e415e5e8d496c3935b
Renata Ontiveros <ontiverosrenata5@gmail.com>
2025-04-09 16:13:15 +0000

    !=

--- variables-operations.c
+++ variables-operations.c
@@ -5,10 +5,14 @@ int main(){
     int area = b * h;
     int perimeter = b*2+h*2;
     printf("Area = %d \n", area);
+    scanf("%d", &area);
     printf("Perimeter = %d \n",perimeter);
+    scanf("%d", &perimeter);
     int farenheit = 18;
 int celsius = (farenheit-32)/1.8;
 printf("%d°F = %d°C\n",farenheit,celsius);
+scanf("%d", &farenheit);
+scanf("%d", &celsius);
 int minutes = 349;
 int modulus = 349%60;
 int hours = 349/60;
@@ -17,6 +21,7 @@ int radio = 8;
 const float pi = 3.14159265359;
 int circleArea = pi * (radio*radio);
 printf("Area of a Circle = %d \n", circleArea);
+scanf("%d",&circleArea);
 
 
     return 0;
