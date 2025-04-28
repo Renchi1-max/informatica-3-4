dac6329b6bafc035e764828becd211c3669a8da6
Renata Ontiveros <ontiverosrenata5@gmail.com>
2025-04-28 16:30:30 +0000

    challenge

--- variables-operations.c
+++ variables-operations.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 
 int main(){
-    int b = 2, h = 10;
-    int area = b * h;
-    int perimeter = b*2+h*2;
+    int height;
+    int lenght;
+    printf("Enter the rectangle height:");
+    scanf("%d" ,&height);
+    printf("Enter the rectangle lenght");
+    scanf("%d",&lenght);
+    int area =height*lenght;
+    int perimeter = height+height+lenght+lenght;
     printf("Area = %d \n", area);
-    scanf("%d", &area);
     printf("Perimeter = %d \n",perimeter);
-    scanf("%d", &perimeter);
-    int farenheit = 18;
+
+int farenheit = 18;
 int celsius = (farenheit-32)/1.8;
 printf("%d°F = %d°C\n",farenheit,celsius);
 scanf("%d", &farenheit);
 scanf("%d", &celsius);
+
 int minutes = 349;
 int modulus = 349%60;
 int hours = 349/60;
@@ -23,7 +28,6 @@ int circleArea = pi * (radio*radio);
 printf("Area of a Circle = %d \n", circleArea);
 scanf("%d",&circleArea);
 
-
-    return 0;
+return 0;
 
 }
