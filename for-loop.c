912b2566111e662d9dae5f752d11fc1bdb2b16e5
Renata Ontiveros <ontiverosrenata5@gmail.com>
2025-04-30 17:33:31 +0000

    chart

--- for-loop.c
+++ for-loop.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
 int main(){
+
+    //Exercise 1
+    int n;
+    printf("Enter a positive number:");
+    scanf(" %d",&n);
     
+    int m;
+    int sum = 0;
+    for (m = 1; m < n + 1; ++m){
+        sum = sum + m;
+    }
+    printf(" %d \n",sum);
+    //Exercise 2
+    int o;
+    printf("Enter a positive number:");
+    scanf(" %d",&o);
+
+    int multiplication=1;
+    
+
+
 }
