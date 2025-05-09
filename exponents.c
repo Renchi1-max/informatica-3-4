a5e7e99417ced2d0e9b2e752cba15a1b7e1899cb
Renata Ontiveros <ontiverosrenata5@gmail.com>
2025-05-09 14:50:09 +0000

    exponents1

---
+++ exponents.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    int base;
+    int exponent;
+    int count = 1;
+    
+    printf("Enter the base number:");
+    scanf(" %d", &base);
+    printf("Enter the exponent:");
+    scanf(" %d", &exponent);
+
+    int result = base;
+
+
+    do {
+        result = base * result;
+        count++;
+    } while (count <= exponent - 1);
+
+    printf("%d ^ %d = %d\n", base, exponent, result);
+
+
+
+}
