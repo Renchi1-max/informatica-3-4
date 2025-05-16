973391313a907fafe9a8fd77f8de623b63278bb4
Renata Ontiveros <ontiverosrenata5@gmail.com>
2025-05-16 19:41:35 +0000

    random

---
+++ random.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    srand(time(NULL));
+    int rolls;
+    printf("Enter the number of dice rolls:");
+    scanf("%d", &rolls);
+    for(;rolls > 0; -- rolls) {
+    int diceRoll = (rand() % 6) + 1;
+    printf("You rolled a: %d\n", diceRoll); }
+    return 0;
+
+}
